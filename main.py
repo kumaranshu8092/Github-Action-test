42735f2b371b653c0fe7a395dad2af0db90bd268
Sudarshan Kumar <kumaranshu8092@gmail.com>
2024-05-23 00:44:18 +0530

    modified main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Hello World")
 
 
 def suvi():
-    print("Hello Suvi, Please add the me in your companies board member")
+    print("Hello Suvi, Please add the me in your companies board members")
 
 
 suvi()
